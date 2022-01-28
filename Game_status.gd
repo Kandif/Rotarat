f0b77752cba467fb61ed505a5425e2c6b81b8c50
Kandif <krzysztof.klak@2.pl>
2022-01-28 19:54:11 +0100

    asdf

--- Game_status.gd
+++ Game_status.gd
@@ -4,7 +4,17 @@ var finish = [false,false]
 
 var move = [false,false]
 
+var died = [false,false]
+var left_screen = true
 
+var rats_ref = {
+	0: null,
+	1: null
+}
+
+func set_died():
+	for rat in rats_ref:
+		rats_ref[rat].reset()
 
 func _ready():
 	pass
--- scripts/Rat.gd
+++ scripts/Rat.gd
@@ -7,19 +7,27 @@ var player_id = 1
 onready var camera = $cam
 onready var player = $Player
 
+var start_position = Vector2.ZERO
+var start_rotation = 0
+
+onready var tiles:TileMap = $"../TileMap"
+
+var dirc = [["left","right","up","down"],["right","left","down","up"]]
+
+var move = false
+
 var ray = {}
 
 func _ready():
-	print($"../TileMap".get_cell(13, 8))
-	print($"../TileMap".get_cell(15, 8))
-
-
-
-	$Player/ref_l.visible = false
-	$Player/ref_r.visible = false
+	start_position = player.global_position
 	if left_side:
+		$Player/anmt.animation = "right"
+		GameStatus.rats_ref[0] = self
+		$Player/ref_l.position = player.position + Vector2(0,285)
+		$Player/ref_r.position = player.position + Vector2(0,-285)
 		fix = -1
-		$Player/Sprite.position.x -= 285
+		$Player/anmt.position.x -= 285
+		$Player/anmt.modulate = Color.pink
 		$Player/CollisionShape2D.position.x -= 285
 		$"Player/action-colision".position.x -= 285
 		ray = {
@@ -28,10 +36,15 @@ func _ready():
 			"up" : get_node("Player/action-colision/up"),
 			"down" : get_node("Player/action-colision/down")
 		}
-	else:
+	else:	
+		$Player/anmt.animation = "left"
+		GameStatus.rats_ref[1] = self
+		$Player/anmt.modulate = Color.deepskyblue
+		$Player/ref_l.position = player.position + Vector2(0,285)
+		$Player/ref_r.position = player.position + Vector2(0,-285)
 		player_id = 2
 		fix = 1
-		$Player/Sprite.position.x += 285
+		$Player/anmt.position.x += 285
 		$Player/CollisionShape2D.position.x += 285
 		$"Player/action-colision".position.x += 285
 		ray = {
@@ -45,35 +58,58 @@ func _process(delta):
 	$cam.position = $Player.position
 
 func _input(event):
-
-
-
-#	print(ray.left.is_colliding())
-	if event.is_pressed() && event is InputEventMouseButton && event.button_index == (player_id) && !$Tween.is_active():
-		$Tween.interpolate_property(camera,"rotation_degrees",camera.rotation_degrees,camera.rotation_degrees+90,0.5)
-		$Tween.interpolate_property(player,"rotation_degrees",player.rotation_degrees,player.rotation_degrees+90,0.5)
-		$Player/CollisionShape2D.disabled = true
-		$Tween.start()
-
+	if event.is_pressed() && event is InputEventMouseButton && !$Tween.is_active() && GameStatus.left_screen==left_side:
+		if event.button_index == (1):
+			player.get_node("anmt").frame=0
+			$Tween.interpolate_property(camera,"rotation_degrees",camera.rotation_degrees,camera.rotation_degrees+90,0.5)
+			$Tween.interpolate_property(player,"rotation_degrees",player.rotation_degrees,player.rotation_degrees+90,0.5)
+			$Player/CollisionShape2D.disabled = true
+			$Tween.start()
+		elif event.button_index == (2):	
+			player.get_node("anmt").frame=0
+			$Tween.interpolate_property(camera,"rotation_degrees",camera.rotation_degrees,camera.rotation_degrees-90,0.5)
+			$Tween.interpolate_property(player,"rotation_degrees",player.rotation_degrees,player.rotation_degrees-90,0.5)
+			$Player/CollisionShape2D.disabled = true
+			$Tween.start()
+		
 	if event.is_pressed() && (event is InputEventKey) && event.scancode == KEY_UP && !$Tween.is_active() && !ray.up.is_colliding():
+		player.get_node("anmt").frame=0
+		player.get_node("anmt").play(dirc[player_id-1][2])
 		$Tween.interpolate_property(player,"position",player.position,player.position+Vector2(0, fix*95).rotated(player.rotation),0.5)
 		$Tween.start()
 	if event.is_pressed() && (event is InputEventKey) && event.scancode == KEY_LEFT && !$Tween.is_active() && !ray.left.is_colliding():
+		player.get_node("anmt").frame=0
+		player.get_node("anmt").play(dirc[player_id-1][0])
 		$Tween.interpolate_property(player,"position",player.position,player.position+Vector2(fix*95, 0).rotated(player.rotation),0.5)
 		$Tween.start()
-	if event.is_pressed() && (event is InputEventKey) && event.scancode == KEY_RIGHT && !$Tween.is_active() && !ray.right.is_colliding():
+	if event.is_pressed() && (event is InputEventKey) && event.scancode == KEY_RIGHT && !$Tween.is_active() && !ray.right.is_colliding():	
+		player.get_node("anmt").frame=0
+		player.get_node("anmt").play(dirc[player_id-1][1])
 		$Tween.interpolate_property(player,"position",player.position,player.position+Vector2(-fix*95, 0).rotated(player.rotation),0.5)
 		$Tween.start()
 	if event.is_pressed() && (event is InputEventKey) && event.scancode == KEY_DOWN && !$Tween.is_active() && !ray.down.is_colliding():
+		player.get_node("anmt").frame=0
+		player.get_node("anmt").play(dirc[player_id-1][3])
 		$Tween.interpolate_property(player,"position",player.position,player.position+Vector2(0, -fix*95).rotated(player.rotation),0.5)
 		$Tween.start()
-
 
 func _on_Tween_tween_all_completed():
 	$Player/CollisionShape2D.disabled = false
 	GameStatus.move[player_id-1] = false
-	pass # Replace with function body.
-
+	var id_tile = tiles.get_cellv(tiles.world_to_map(tiles.to_local(player.get_node("anmt").global_position)))
+	
+	if id_tile == -1:
+		GameStatus.set_died()
+	
+	
+func reset():
+	$Tween.interpolate_property(player,"global_position",player.global_position,start_position,1)
+	$Tween.interpolate_property(player,"rotation",player.rotation,0,1)
+	$Tween.interpolate_property(camera,"rotation",camera.rotation,0,1)
+	$Tween.start()
+#	player.global_position = start_position
+#	player.rotation = 0
+#	camera.rotation = 0
 
 func _on_actioncolision_area_entered(area):
 	if area.is_in_group("finish"):
@@ -86,4 +122,11 @@ func _on_actioncolision_area_exited(area):
 
 
 func _on_Tween_tween_started(object, key):
+	if player.get_node("anmt").animation == "up":
+		player.get_node("anmt").offset.y=250
+		player.get_node("anmt").offset.x=-50
+	else:
+		player.get_node("anmt").offset.y=0
+		player.get_node("anmt").offset.x=0
 	GameStatus.move[player_id-1] = false
+	move = true
